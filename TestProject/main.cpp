485e71bee2ae8e42b37d58607d9571898f77fd6f
CycleDM <cycledm0508@gmail.com>
2020-10-24 13:49:11 +0900

    添加项目文件。

---
+++ TestProject/main.cpp
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	int num[8] = { 6, 7, 8, 1, 3, 4, 5, 2 };
+	int temp;
+
+	int length = sizeof(num) / sizeof(num[0]);
+	for (int i = 0; i < length; i++)
+	{
+		for (int j = i + 1; j < length; j++)
+		{
+			if (num[j] < num[i])
+			{
+				temp = num[i];
+				num[i] = num[j];
+				num[j] = temp;
+			}
+		}
+		printf_s("%d ", num[i]);
+	}
+	
+	rewind(stdin);
+	getchar();
+	return 0;
+}
