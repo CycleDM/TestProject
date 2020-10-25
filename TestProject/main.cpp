96a1743c3454309591013e252d09d77348258857
CycleDM <cycledm0508@gmail.com>
2020-10-25 22:41:18 +0900

    Update main.cpp

--- TestProject/main.cpp
+++ TestProject/main.cpp
@@ -3,7 +3,7 @@
 
 void main()
 {
-	int num[8] = { 411, 305, 353, 341, 354, 44, 512, 22 };
+	int num[8] = { 411, 305, 53, 31, 354, 44, 52, 22 };
   int temp;
 
 	int length = sizeof(num) / sizeof(num[0]);
